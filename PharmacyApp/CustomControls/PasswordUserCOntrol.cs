d617bc2285131948fe5bda91cb642571069ed9b0
MrBoringChenk <pro100den4ik023@gmail.com>
2021-10-12 12:33:20 +0500

    2 обновление

---
+++ PharmacyApp/CustomControls/PasswordUserCOntrol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PharmacyApp.CustomControls
+{
+    public class PasswordUserControl : UserControl
+    {
+        public SecureString Password
+        {
+            get { return (SecureString)GetValue(PasswordProperty); }
+        }
+        //public static readonly DependencyProperty PasswordProperty = 
+        //    DependencyProperty.Register("Password", typeof(SecureString)
+        //        , typeof)
+    }
+}
