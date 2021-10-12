2e7eac3ecc16012901c3608c9ac84cea873b2e2c
MrBoringChenk <pro100den4ik023@gmail.com>
2021-10-12 12:22:32 +0500

    Первое обновление

---
+++ PharmacyApp/ViewModels/LiginWindowVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyApp.ViewModels
+{
+    public class LiginWindowVM : BaseVM
+    {
+        private string login;
+        private string password;
+        private bool showPassword;
+
+        public bool ShowPassword { get => showPassword; set { showPassword = value; OnPropertyChanged(); } }
+        public string Password { get => password; set { password = value; OnPropertyChanged(); } }
+        public string Login { get => login; set { login = value; OnPropertyChanged(); } }
+    }
+}
